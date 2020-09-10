908b3e239897e20f6d7d25565435c80c46247b85
trongnn-1033 <nguyen.ngoc.trong@sun-asterisk.com>
2020-09-10 08:49:59 +0700

    initial

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 
 import Home from 'containers/Home';
 import { withTranslation } from 'utils/with-i18next';
+import { getShowcases } from 'containers/Home/actions';
+import reducer from 'containers/Home/reducer';
+import saga from 'containers/Home/saga';
 
+const key = 'showcases';
 export class IndexPage extends React.PureComponent {
   render() {
     const { t } = this.props;
@@ -16,8 +20,11 @@ IndexPage.propTypes = {
   t: PropTypes.func,
 };
 
-IndexPage.getInitialProps = async () => ({
-  namespacesRequired: ['common', 'banner', 'features'],
-});
+IndexPage.getInitialProps = async ({ store }) => {
+  console.log(store);
+  return {
+    namespacesRequired: ['common', 'banner', 'features'],
+  };
+};
 
 export default withTranslation('common')(IndexPage);
